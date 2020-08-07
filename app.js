4f406147cc250e672109d845aa6e96414b8b6b85
Andrew Cacayuran <andrew.cacayuran@Ultras-MacBook-Pro.local>
2020-08-07 09:01:27 -0700

    fixed js

--- app.js
+++ app.js
@@ -9,18 +9,18 @@ const gradientChange = "linear-gradient(to right, #5637dd, #3046c5, #60106b)";
 const dark = document.getElementById("darkSwitch");
 const storage = localStorage.getItem("darkSwitch");
 
-inDarkMode(storage, textElement,navElement,gradientChange)
+inDarkModeInit(storage, textElement,navElement,gradientChange)
 
-function changedMe(text, navBar, grad){
+function changedMe(text, navBar, gradient){
   return {
-    first: Array.from(navBar).forEach(el => el.style.background = grad),
+    first: Array.from(navBar).forEach(el => el.style.background = gradient),
     second: Array.from(text).forEach(el => el.style.color = 'black')
 
   }
 }
 
 
-function inDarkMode(storage, text, nav, gradient){
+function inDarkModeInit(storage, text, nav, gradient){
   if(!storage === 'dark') {
     dark.addEventListener('change', changedMe(text, nav, gradient))
   } else {
