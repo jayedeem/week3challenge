924fe11b095e605b4640359743cdf6220f858ed0
Andrew Cacayuran <andrew.cacayuran@Ultras-MacBook-Pro.local>
2020-08-06 18:56:04 -0700

    fixed darkmode functionality and add styling

--- app.js
+++ app.js
@@ -3,22 +3,38 @@ $(function () {
   $(`[data-toggle='tooltip']`).tooltip();
 });
 
-const darkMode = document
+
+const text = document.getElementsByClassName('whiteText');
+const navBar = document.getElementsByClassName('navbar')
+const gradient = 'linear-gradient(to right, #5637dd, #3046c5, #60106b)'
+
+const storage = localStorage.getItem("darkSwitch")
+
+const darkMode = () => document
   .getElementById('darkSwitch')
   .addEventListener('change', () => {
     const text = document.getElementsByClassName('whiteText');
-    const bsImg = document.getElementById('bootStrapImg');
-    bsImg.style = {
-      background: '#111',
-    };
-    Array.from(text).forEach((el) => (el.style.color = 'black'));
+    const navBar = document.querySelector('.navbar')
+    
+    return {
+      first: navBar.style.background = gradient,
+      second: Array.from(text).forEach((el) => (el.style.color = 'black'))
+    }
   });
 
-// const darkMode = document
-// .getElementById('darkSwitch')
-// .addEventListener('change', () => {
-//   const text = document.getElementsByClassName('whiteText');
-//   const style = document.createElement('style');
 
-//   Array.from(text).forEach((el) => (el.style.color = 'black'));
-// });
+function isDark(){
+  if(storage === 'dark') {
+    const text = document.getElementsByClassName('whiteText');
+      const navBar = document.querySelector('.navbar')
+      
+      return {
+        first: navBar.style.background = gradient,
+        second: Array.from(text).forEach((el) => (el.style.color = 'black'))
+      }
+  } else {
+    darkMode()
+  }
+}
+
+isDark()
